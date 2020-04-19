90f14c880f934eaa33b473c0569433fd11ce1666
Teñikaler Smith <tenikaler.smith@utp.ac.pa>
2020-04-19 00:00:26 -0500

    init

---
+++ wheather-app/src/components/Location.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Location = () => (
+    <div><h1>Panamá</h1></div>
+);
+
+export default Location;
---
+++ wheather-app/src/components/WeatherData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
+
+const WeatherData = () => (
+    <div>
+        <WeatherTemperature></WeatherTemperature>
+        <WeatherExtraInfo></WeatherExtraInfo>
+        
+    </div>
+);
+
+export default WeatherData;
